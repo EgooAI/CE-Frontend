018d7b2228330437e732a19f381ffc0aa2da55fc
yuanzhidao <yuanzhidao@outlook.com>
2025-12-03 23:01:10 +0800

    feat/reminderTmp

---
+++ lib/services/reminder_service.dart
@@ -0,0 +1,16 @@
+import 'package:dio/dio.dart';
+import '../models/reminder.dart';
+import 'api_client.dart';
+
+/// 提醒服务
+class ReminderService {
+  /// 获取所有提醒
+  Future<RemindersResponse> getReminders() async {
+    try {
+      final response = await ApiClient.instance.get('/schedules/reminders');
+      return RemindersResponse.fromJson(response.data);
+    } on DioException catch (e) {
+      throw Exception('获取提醒列表失败: ${e.message}');
+    }
+  }
+}
